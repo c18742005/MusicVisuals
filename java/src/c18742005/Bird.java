a75e4a074a4e6b60731f69ae27ce9992ffe19723
C18742005 <c18742005@mytudublin.ie>
2020-05-05 01:28:20 +0100

    Fixed an issue with static field constants in the Bird class

--- java/src/c18742005/Bird.java
+++ java/src/c18742005/Bird.java
@@ -34,10 +34,10 @@ public class Bird
 
             pa.ellipse(x - 30, y - 0.5f, 40, 40);  // head
             pa.triangle(x - 110, y + 19.5f , x - 140, y + 19.5f, x - 140, y + 44.5f); // tail
-            pa.arc(x - 70, y + 19.5f, 80, 80, 0, pa.PI, pa.OPEN); // body
+            pa.arc(x - 70, y + 19.5f, 80, 80, 0, PApplet.PI, PApplet.OPEN); // body
 
             pa.stroke(0);
-            pa.arc(x - 90, y + 29.5f, 50, 50, 0, pa.PI, pa.CHORD); // wing
+            pa.arc(x - 90, y + 29.5f, 50, 50, 0, PApplet.PI, PApplet.CHORD); // wing
 
             pa.fill(25, 255, 255);
             pa.triangle(x - 12, y - 8, x - 12, y + 7, x, y); // beak
@@ -54,10 +54,10 @@ public class Bird
 
             pa.ellipse(pa.mouseX + 30, y - 0.5f, 40, 40);  // head
             pa.triangle(pa.mouseX + 110, y + 19.5f , pa.mouseX + 140, y + 19.5f, pa.mouseX + 140, y + 44.5f); // tail
-            pa.arc(pa.mouseX + 70, y + 19.5f, 80, 80, 0, pa.PI, pa.OPEN); // body
+            pa.arc(pa.mouseX + 70, y + 19.5f, 80, 80, 0, PApplet.PI, PApplet.OPEN); // body
 
             pa.stroke(0);
-            pa.arc(pa.mouseX + 90, y + 29.5f, 50, 50, 0, pa.PI, pa.CHORD); // wing
+            pa.arc(pa.mouseX + 90, y + 29.5f, 50, 50, 0, PApplet.PI, PApplet.CHORD); // wing
 
             pa.fill(25, 255, 255);
             pa.triangle(pa.mouseX + 12, y - 8, pa.mouseX + 12, y + 7, pa.mouseX, y); // beak
@@ -87,10 +87,10 @@ public class Bird
 
             pa.ellipse(birdX - 30, birdY - 0.5f, 40, 40);  // head
             pa.triangle(birdX - 110, birdY + 19.5f , birdX - 140, birdY + 19.5f, birdX - 140, birdY + 44.5f); // tail
-            pa.arc(birdX - 70, birdY + 19.5f, 80, 80, 0, pa.PI, pa.OPEN); // body
+            pa.arc(birdX - 70, birdY + 19.5f, 80, 80, 0, PApplet.PI, PApplet.OPEN); // body
 
             pa.stroke(0);
-            pa.arc(birdX - 90, birdY + 29.5f, 50, 50, 0, pa.PI, pa.CHORD); // wing
+            pa.arc(birdX - 90, birdY + 29.5f, 50, 50, 0, PApplet.PI, PApplet.CHORD); // wing
 
             pa.fill(25, 255, 255);
             pa.triangle(birdX - 12, birdY -8, birdX -12, birdY + 7, birdX, birdY); // beak
@@ -106,10 +106,10 @@ public class Bird
 
             pa.ellipse(birdX + 30, birdY - 0.5f, 40, 40);  // head
             pa.triangle(birdX + 110, birdY + 19.5f , birdX + 140, birdY + 19.5f, birdX + 140, birdY + 44.5f); // tail
-            pa.arc(birdX + 70, birdY + 19.5f, 80, 80, 0, pa.PI, pa.OPEN); // body
+            pa.arc(birdX + 70, birdY + 19.5f, 80, 80, 0, PApplet.PI, PApplet.OPEN); // body
 
             pa.stroke(0);
-            pa.arc(birdX + 90, birdY + 29.5f, 50, 50, 0, pa.PI, pa.CHORD); // wing
+            pa.arc(birdX + 90, birdY + 29.5f, 50, 50, 0, PApplet.PI, PApplet.CHORD); // wing
 
             pa.fill(25, 255, 255);
             pa.triangle(birdX + 12, birdY -8, birdX + 12, birdY + 7, birdX, birdY); // beak
